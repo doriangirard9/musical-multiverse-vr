952468843136db67cd89e753e1c98cea6a71008d
Dorian <98755570+Doori4N@users.noreply.github.com>
2024-05-27 17:53:08 +0200

    fixed json fetching

--- src/audioNodes3D/AudioNode3DBuilder.ts
+++ src/audioNodes3D/AudioNode3DBuilder.ts
@@ -13,8 +13,12 @@ export class AudioNode3DBuilder {
 
     public async create(name: string, id: string, configFile?: string): Promise<AudioNode3D> {
         if (name === "simpleOscillator") {
-            // @ts-ignore
-            const config: IAudioNodeConfig = await import(/* @vite-ignore */`${WAM_CONFIGS_URL}/coreConfig/simpleOscillatorConfig.json`);
+            const response: Response = await fetch(`${WAM_CONFIGS_URL}/coreConfig/simpleOscillatorConfig`, {
+                method: "get",
+                headers: {"Content-Type": "application/json"}
+            });
+            const configString: string = await response.json();
+            const config: IAudioNodeConfig = JSON.parse(configString);
             return new SimpleOscillator3D(this._scene, this._audioCtx, id, config);
         }
         else if (name === "stepSequencer") {
@@ -25,7 +29,12 @@ export class AudioNode3DBuilder {
         }
         // WAMs
         else {
-            const config: IWamConfig = await import(/* @vite-ignore */`${WAM_CONFIGS_URL}/wamsConfig/${configFile}.json`);
+            const response: Response = await fetch(`${WAM_CONFIGS_URL}/wamsConfig/${configFile}`, {
+                method: "get",
+                headers: {"Content-Type": "application/json"}
+            });
+            const configString: string = await response.json();
+            const config: IWamConfig = JSON.parse(configString);
             return new Wam3D(this._scene, this._audioCtx, id, config, configFile!);
         }
     }
