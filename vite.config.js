49ceb73903f652cd82968af06eb70811542c1425
fletox <fletoxishere@gmail.com>
2024-06-10 14:11:22 +0200

    adding ssh certificat for dev purpose

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
+
+export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.crt')),
+    },
+  },
+});
