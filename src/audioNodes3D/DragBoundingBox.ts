e9f08df465b5576cdb6336bfc52c32e7aac7249c
ayoub <fletoxishere@gmail.com>
2024-07-17 17:26:48 +0200

    fix bug dragging boundingBox

--- src/audioNodes3D/DragBoundingBox.ts
+++ src/audioNodes3D/DragBoundingBox.ts
@@ -1,23 +1,50 @@
-
 import * as B from "@babylonjs/core";
-import { App } from "../App.ts";
-
+import { App } from "../App";
 
 export class DragBoundingBox implements B.Behavior<B.AbstractMesh> {
-    
-    name = "test";
+    name = "DragBoundingBox";
     selected: B.AbstractMesh | null = null;
     drag: B.PointerDragBehavior;
 
     constructor(private app: App) {
+        // Initialize drag behavior with a default normal
         this.drag = new B.PointerDragBehavior({ dragPlaneNormal: new B.Vector3(0, 0, 1) });
-
-        // const data = this.app._getPlayerState();
-        // const norm = new B.Vector3(data.direction.x, 0, data.direction.z);
-        // console.log(norm)
-        // this.drag = new B.PointerDragBehavior({ dragPlaneNormal: norm });
     }
 
+    init(): void {
+        // Add controller listeners if applicable, for example in XR environments
+        this.app.xrManager.xrHelper.input.controllers.forEach(controller => {
+            const thumbstick = controller.motionController?.getComponent("xr-standard-thumbstick");
+            thumbstick?.onAxisValueChangedObservable.add((axis) => {
+                if (this.selected) {
+                    // Update drag normal dynamically based on player state
+                    const data = this.app._getPlayerState();
+                    const norm = new B.Vector3(data.direction.x, 0, data.direction.z);
+                    this.updateDragBehavior(norm, axis.y);
+                }
+            });
+        });
+    }
+
+    attach(target: B.AbstractMesh): void {
+        target.actionManager = new B.ActionManager(target._scene);
+        target.actionManager.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPickDownTrigger, () => {
+            this.select(target);
+        }));
+        target.actionManager.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPickUpTrigger, () => {
+            this.onRelease(target);
+        }));
+        target.actionManager.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPickOutTrigger, () => {
+            this.onRelease(target);
+        }));
+    }
+
+    detach(): void {
+        if (this.selected) {
+            this.select(null);
+        }
+        // target.actionManager.clear();
+    }
     select(target: B.AbstractMesh | null) {
         console.log("selected");
         if (!this.selected) this.app.xrManager.xrFeaturesManager.disableFeature(B.WebXRFeatureName.MOVEMENT);
@@ -31,44 +58,26 @@ export class DragBoundingBox implements B.Behavior<B.AbstractMesh> {
         }
     }
 
-    init(): void {
-        this.app.xrManager.xrHelper.input.controllers.forEach(controller => {
-            const thumbstick = controller.motionController?.getComponent("xr-standard-thumbstick");
-            thumbstick?.onAxisValueChangedObservable.add((axis) => {
-                if(this.selected){
-                    const data = this.app._getPlayerState();
-                    const norm = new B.Vector3(data.direction.x,data.direction.y,data.direction.z)            
-                    this.selected.removeBehavior(this.drag);
-                    this.selected.position.addInPlace(norm.scaleInPlace(axis.y*-0.3));
-                    // this.drag.attach(this.selected!);
-                    this.selected.addBehavior(this.drag);
-                }
-            });
-        });
-    }
 
-    attach(target: B.AbstractMesh): void {
-        target?.actionManager?.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPickDownTrigger, () => {
-             this.select(target);
-        }));
 
-        const on_up = () => {
-            if (this.selected) this.app.xrManager.xrFeaturesManager.enableFeature(B.WebXRFeatureName.MOVEMENT, "latest", {
+    onRelease(target: B.AbstractMesh): void {
+        if (this.selected) {
+            this.app.xrManager.xrFeaturesManager.enableFeature(B.WebXRFeatureName.MOVEMENT, "latest", {
                 xrInput: this.app.xrManager.xrHelper.input,
                 movementSpeed: 0.2,
                 rotationSpeed: 0.3,
             });
-            this.selected?.removeBehavior(this.drag);
+            this.selected.visibility = 0;
+            this.selected.removeBehavior(this.drag);
             this.selected = null;
-            target.visibility = 0;
-        };
-
-        target?.actionManager?.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPickUpTrigger, on_up));
-        target?.actionManager?.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPickOutTrigger, on_up));
+        }
     }
 
-    detach(): void {
-        console.log("detach");
-    this.select(null);
+    private updateDragBehavior(norm: B.Vector3, scale: number): void {
+        if (this.selected) {
+            this.selected.removeBehavior(this.drag);
+            this.selected.position.addInPlace(norm.scaleInPlace(scale * -0.3));
+            this.selected.addBehavior(this.drag);
+        }
     }
 }
