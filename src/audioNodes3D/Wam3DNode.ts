da30246bbe087b577767783c58a5d4447fa212ee
Jempasam <samueldemont83@gmail.com>
2025-03-28 14:27:45 +0100

    Parameters all handleds correctly

--- src/audioNodes3D/Wam3DNode.ts
+++ src/audioNodes3D/Wam3DNode.ts
@@ -4,9 +4,10 @@ import {Scene} from "@babylonjs/core";
 import { controls, WamGUIGenerator, WAMGuiInitCode } from "wam3dgenerator";
 import { AudioNode3D } from "./AudioNode3D.ts";
 import { AudioNodeState } from "../network/types.ts";
-import { WamNode } from "@webaudiomodules/api";
+import { WamNode, WamParameterInfo } from "@webaudiomodules/api";
 import { App } from "../App.ts";
 import { BoundingBox } from "./BoundingBox.ts";
+import { AdvancedDynamicTexture, TextBlock } from "@babylonjs/gui";
 
 export class Wam3DNode extends AudioNode3D {
     
@@ -27,6 +28,8 @@ export class Wam3DNode extends AudioNode3D {
         const transform = new B.TransformNode("root", this._scene)
 
         this._app.menu.hide()
+
+        const highlightLayer = new B.HighlightLayer(`hl-output-${node3d.id}`, node3d._scene)
 
         this._wam_generator = await WamGUIGenerator.create_and_init(
             {
@@ -64,9 +67,7 @@ export class Wam3DNode extends AudioNode3D {
                     node3d._output_audio_node = settings.node
                     node3d.outputMesh = target as B.Mesh
                     target.actionManager = new B.ActionManager(node3d._scene)
-                        
-                    const highlightLayer = new B.HighlightLayer(`hl-output-${node3d.id}`, node3d._scene)
-                    
+                                            
                     const color = B.Color3.Red()
                     target.actionManager.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPointerOverTrigger, () => {
                         for(let m of target.getChildMeshes(false)) if(m instanceof B.Mesh) highlightLayer.addMesh(m, color)
@@ -76,20 +77,76 @@ export class Wam3DNode extends AudioNode3D {
                         for(let m of target.getChildMeshes(false)) if(m instanceof B.Mesh) highlightLayer.removeMesh(m)
                         if(target instanceof B.Mesh) highlightLayer.removeMesh(target)
                     }))
-                
-                    target.actionManager.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnLeftPickTrigger, () => {
-                        node3d.ioObservable.notifyObservers({type: 'output', pickType: 'down', node: node3d});
-                    }))
-                    target.actionManager.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPickUpTrigger, () => {
-                        node3d.ioObservable.notifyObservers({type: 'output', pickType: 'up', node: node3d});
-                    }))
-                    target.actionManager.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPickOutTrigger, () => {
-                        node3d.ioObservable.notifyObservers({type: 'output', pickType: 'out', node: node3d});
-                    }))
+            
                     
                 },
                 defineField(settings) {
-                    
+                    const {target} = settings
+
+                    const textValuePlane: B.Mesh = B.MeshBuilder.CreatePlane('textPlane', { size: 1 }, node3d._scene)
+                    textValuePlane.parent = transform
+                    textValuePlane.rotate(B.Axis.X, 0, B.Space.WORLD)
+                    textValuePlane.setEnabled(false)
+
+                    const valueAdvancedTexture = AdvancedDynamicTexture.CreateForMesh(textValuePlane)
+                    const textValueBlock = new TextBlock()
+                    textValueBlock.fontSize = 200
+                    textValueBlock.color = 'white'
+                    textValueBlock.outlineColor = 'black'
+                    textValueBlock.outlineWidth = 30
+                    valueAdvancedTexture.addControl(textValueBlock)
+
+                    const color = B.Color3.Blue()
+                    target.actionManager = new B.ActionManager(node3d._scene);
+                    target.actionManager.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPointerOverTrigger, (): void => {
+                        for(let m of target.getChildMeshes(false)) if(m instanceof B.Mesh) highlightLayer.addMesh(m, color)
+                        if(target instanceof B.Mesh) highlightLayer.addMesh(target, color)
+                    }))
+                    target.actionManager.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPointerOutTrigger, (): void => {
+                        for(let m of target.getChildMeshes(false)) if(m instanceof B.Mesh) highlightLayer.removeMesh(m)
+                        if(target instanceof B.Mesh) highlightLayer.removeMesh(target)
+                    }))
+                    target.actionManager.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPickDownTrigger, (): void => {
+                        textValuePlane.setEnabled(true)
+                        textValuePlane.setAbsolutePosition(target.getAbsolutePosition())
+                        textValuePlane.position.y += target.getBoundingInfo().boundingBox.extendSize.y/2
+                        textValueBlock.text = settings.stringify(settings.getValue())
+                    }))
+                    target.actionManager.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPickOutTrigger, (): void => {
+                        textValuePlane.setEnabled(false)
+                    }))
+                    target.actionManager.registerAction(new B.ExecuteCodeAction(B.ActionManager.OnPickUpTrigger, (): void => {
+                        textValuePlane.setEnabled(false)
+                    }))
+
+                    const sixDofDragBehavior = new B.SixDofDragBehavior()
+                    sixDofDragBehavior.disableMovement = true
+                    target.addBehavior(sixDofDragBehavior)
+                                
+                    let startingValue = 0
+                    let stepSize = 0.01
+                    let changeFactor = 0
+                    sixDofDragBehavior.onDragStartObservable.add(() => {
+                        startingValue = settings.getValue()
+                        stepSize = settings.getStepSize()
+                        if(stepSize==0){
+                            stepSize = 0.001
+                            changeFactor = 0.2
+                        }
+                        else{
+                            changeFactor = stepSize*2
+                        }
+
+                    })
+                    sixDofDragBehavior.onDragObservable.add((event: {delta: B.Vector3, position: B.Vector3, pickInfo: B.PickingInfo}): void => {
+                        let newvalue = (startingValue + event.delta.y * changeFactor)
+                        newvalue = newvalue - newvalue % stepSize
+                        newvalue = Math.max(0, Math.min(1, newvalue))
+                        settings.setValue(newvalue)
+                        textValueBlock.text = settings.stringify(newvalue)
+                        textValuePlane.setAbsolutePosition(target.getAbsolutePosition())
+                        textValuePlane.position.y += target.getBoundingInfo().boundingBox.extendSize.y
+                    })
                 },
                 onFieldChange(label, value) {
                     
