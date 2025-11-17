e3eb0607cdcb27bb9a352d184f958c2ccab73099
Adam MIR-SADJADI <117532525+ADMR-S@users.noreply.github.com>
2025-11-17 20:00:31 +0100

    fix drum glb path

--- src/Refactoring/node3d/subs/drumkit/XRDrumKit/XRDrumKitConfig.ts
+++ src/Refactoring/node3d/subs/drumkit/XRDrumKit/XRDrumKitConfig.ts
@@ -106,7 +106,7 @@ export const DRUMKIT_CONFIG = {
     
     // Drum kit 3D model configuration
     model: {
-        path: "/drum_3D_model/",
+        path: "./src/Refactoring/node3d/subs/drumkit/",
         fileName: "drum3DModel.glb",
         scaleFactor: 0.7, // Overall scale for the entire drum kit
     },
