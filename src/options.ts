1aa21ba37e03fdb5ca1e378c9a364248faec12e8
Jempasam <samueldemont83@gmail.com>
2026-05-29 01:15:44 +0200

    some updates

--- src/options.ts
+++ src/options.ts
@@ -1,4 +1,5 @@
+export const SERVER_NAME = import.meta.env.VITE_SERVER_NAME
+    ?? 'http://localhost:3000';
 
-export const SERVER_NAME = 'https://wamjamparty.i3s.univ-cotedazur.fr/config';
-
-export const SIGNALING_SERVER = `https://wamjamparty.i3s.univ-cotedazur.fr/rtc`;
+export const SIGNALING_SERVER = import.meta.env.VITE_SIGNALING_SERVER
+    ?? `https://wamjamparty.i3s.univ-cotedazur.fr/rtc`;
